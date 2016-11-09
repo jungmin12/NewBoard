6a08caa49c243c3a59a57f3e07456e9283b4daef
user <user@user-PC>
2016-11-09 09:59:45 +0900

    s

--- src/org/zerock/dao/BoardDAO.java
+++ src/org/zerock/dao/BoardDAO.java
@@ -1,95 +1,15 @@
 package org.zerock.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.zerock.domain.BoardVO;
 
-public enum BoardDAO {
-	INSTANCE;
+public interface BoardDAO {
 
-	public void create(BoardVO vo) throws Exception {
+	public void create(BoardVO vo) throws Exception;
 
-		String sql = "insert into tbl_board(title,content,writer) values(?,?,?)";
+	public BoardVO read(Integer bno) throws Exception;
 
-		new ConnectDAO() {
+	public void update(BoardVO vo) throws Exception;
 
-			@Override
-			public void doExecute() throws Exception {
-				pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, vo.getTitle());
-				pstmt.setString(2, vo.getContent());
-				pstmt.setString(3, vo.getWriter());
-				pstmt.executeUpdate();
-
-			}
-		}.execute();
-
-	}
-
-	public BoardVO read(Integer bno) throws Exception {
-
-		String sql = "select * from tbl_board where bno = ?";
-		List<BoardVO> resultArr = new ArrayList<>();
-		new ConnectDAO() {
-
-			@Override
-			public void doExecute() throws Exception {
-				pstmt = con.prepareStatement(sql);
-				pstmt.setInt(1, bno);
-				rs = pstmt.executeQuery();
-
-				while (rs.next()) {
-					BoardVO vo = new BoardVO();
-					vo.setBno(rs.getInt("bno"));
-					vo.setTitle(rs.getString("title"));
-					vo.setContent(rs.getString("content"));
-					vo.setWriter(rs.getString("writer"));
-
-					resultArr.add(vo);
-
-				}
-			}
-		}.execute();
-
-		return resultArr.get(0);
-
-	}
-
-	public void update(BoardVO vo) throws Exception {
-		String sql = "UPDATE tbl_board SET `content`=? WHERE `bno`=?";
-
-		new ConnectDAO() {
-
-			@Override
-			public void doExecute() throws Exception {
-				pstmt = con.prepareStatement(sql);
-
-				pstmt.setString(1, vo.getContent());
-				pstmt.setInt(2, vo.getBno());
-
-				pstmt.executeUpdate();
-
-			}
-		}.execute();
-
-	}
-
-	public void delete(Integer bno) throws Exception {
-
-		String sql = "delete from tbl_board where bno =?";
-		new ConnectDAO() {
-
-			@Override
-			public void doExecute() throws Exception {
-				pstmt = con.prepareStatement(sql);
-
-				pstmt.setInt(1, bno);
-
-				pstmt.executeUpdate();
-
-			}
-		}.execute();
-	}
+	public void delete(Integer bno) throws Exception;
 
 }
--- src/org/zerock/test/MySqlTest.java
+++ src/org/zerock/test/MySqlTest.java
@@ -9,32 +9,14 @@ import org.zerock.domain.BoardVO;
 
 public class MySqlTest {
 
-	
-	BoardVO vo;
-	
-	BoardDAO dao;
 	@Test
-	public void test() throws Exception {
-		vo = new BoardVO(); 
+	public void testConnection() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://192.168.0.129:3306/bit88?useSSL=false", "makemoney",
+				"makemoney");
 
-	
-		vo.setContent("엄준호");
-		vo.setBno(8);
-		
-		dao.INSTANCE.delete(9);
-		
-	
-	
+		System.out.println(con);
+
+		con.close();
 	}
-
-	@Test
-	   public void testConnection()throws Exception{
-	      Class.forName("com.mysql.jdbc.Driver");
-	      Connection con = DriverManager.getConnection(
-	            "jdbc:mysql://192.168.0.129:3306/bit88?useSSL=false","makemoney","makemoney");
-	      
-	      System.out.println(con);
-	      
-	      con.close();
-	   }
 }
